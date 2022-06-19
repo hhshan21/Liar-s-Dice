e7746043552b6425c9cefa65800b5f776678db08
Huishan <hui_shan91@hotmail.com>
2022-06-19 15:20:33 +0800

    added CSS for start noti

--- Main/index.js
+++ Main/index.js
@@ -1,10 +1,32 @@
-function startNoti(isShown) {
 
+
+
+// Starting whole game with a start notification
+function startNoti(isShown) {
+    const notiContainerDom = document.getElementById('start-noti');
+
+    if(isShown) {
+        notiContainerDom.style.display='';
+    } else {
+        document.getElementById('start-noti').style.display = 'none';
+    }
+}
+
+// start game after clicking start notification btn
+function startGame() {
+    startNoti(false);
+}
+
+function addEventListeners() {
+    document.getElementById('start-noti').addEventListeners('click', () => {
+        startGame();
+    })
 }
 
 
 function init() {
-
+    startNoti(true);
+    addEventListeners();
 }
 
-init()
+init();
