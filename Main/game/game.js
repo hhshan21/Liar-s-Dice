8e058dec6ed69a8fb65ef3d7301eb4945fd19e9d
Huishan <hui_shan91@hotmail.com>
2022-06-25 17:26:07 +0800

    player bid choices shown in html

--- Main/game/game.js
+++ Main/game/game.js
@@ -3,29 +3,44 @@ import PlayerComponent from "../player-component/player-component.js";
 export default class Game {
     #players = [];
     #currentPlayerTurn = 0; // Index of this.#players
-  
+
     constructor() {
-      // 
+        this.numOfDicePerPlayer = 5;
+        this.totalDiceFace = 6;
+        this.bidChoices = this.createBidChoices()
     }
-    
+
+    createBidChoices() {
+        const bidChoices = [];
+        for (let i = 1; i <= this.numOfDicePerPlayer; i++) {
+            for (let j = 1; j <= this.totalDiceFace; j++) {
+                const diceBid = {
+                    numOfDice: i,
+                    dieFace: j,
+                    // if already selected, dont want it to be selected again
+                    isSelected: false
+                }
+                bidChoices.push(diceBid);
+            }
+        }
+        return bidChoices
+    }
+
     // passed the whole class User into the addPlayer function
     addPlayer(user) {
         this.#players.push(user);
     }
-  
+
     startGame() {
         const computer = document.getElementById('players-not-me');
         const playerMe = document.getElementById('player-me');
-  
+
         let computerDiceMarkup = '';
         let playerDiceMarkup = '';
-        
+
         this.#players.forEach((player, ind) => {
             player.shuffleDice();
-            
-            /* how come the /shuffleDice() function can be called here?
-            how come the PlayerComponent receives an argument of player? 
-            Is it because the PlayerCompanent class object received an argument from user that's why can perform "player.isNpc"?*/
+
             const playerComponent = new PlayerComponent(player);
 
             // if(player.isNpc || this.#currentPlayerTurn !== ind) {
@@ -42,11 +57,11 @@ export default class Game {
                 computerDiceMarkup += playerComponent.render(isPlayer);
             }
         })
-  
+
         computer.innerHTML = computerDiceMarkup;
         playerMe.innerHTML = playerDiceMarkup;
     }
-  
+
     // displayBid(name, chosen) {
     //   const dom = document.getElementById('bid-container');
     //   const str = `${name} bid: ${chosen}`
@@ -56,61 +71,39 @@ export default class Game {
     showMyAvailableBid() {
         const myBidContainer = document.getElementById('my-bid-container');
 
-        // // create a bid options for player
-        // const bidOptions = document.createElement('div');
-        // bidOptions.setAttribute('id', 'bid-options');
-        // myBidContainer.appendChild(bidOptions);
-
-        // // create individual bid options for player to select
-        // const indBidOptions = document.createElement('div');
-        // indBidOptions.setAttribute('class', 'ind-bid-option');
-        // indBidOptions.setAttribute('dice-count', 1);
-        // indBidOptions.setAttribute('dice-face', 1);
-
-        // bidOptions.appendChild(indBidOptions)
-        
-        // indBidOptions.innerHTML = 
-        //     `<span>1 x </span>
-        //     <img src ="./../Images/Dice1.png"/>
-        // `
-        
-        const numOfDicePerPlayer = 5;
-        const numOfPlayer = this.#players.length;
-        const totalDiceFace = 6;
-        
-        const totalNumDiceOptions  = numOfDicePerPlayer * numOfPlayer;
-        
-        const choices = [];
-        
-        for(let i=1; i<=numOfDicePerPlayer; i++) {
-            for(let j=1; j<=totalDiceFace; j++) {
-            const diceBid = {
-                numOfDice: i,
-                dieFace: j,
-                isSelected: false
-            }
-            choices.push(diceBid);
-            }
-        }
-
-        console.log(choices[1].numOfDice)
-        
-        return choices.filter((choice) => choice.isSelected).map(choice => `
-            <div id="bid-options">
+        const bidChoicesMarkup = this.bidChoices
+            .filter((choice) => !choice.isSelected)
+            .map(choice => `
                 <div class="ind-bids">
-                    <span>choices....
-                    <img src="./../Images/Dice1.png"/>
-                </div>
-            </div>
-        `)
-        
-        onclick="updateDiceState(1,2)"
-        
-        function updateDiceState(numOfDice, dieFace) {
-            const choice = choices.find(choice => choice.numOfDice === numOfDice && choice.dieFace === dieFace)
-            choice.isSelected = true;
-            // return choice?
-        }
+                    <span>${choice.numOfDice} x </span>
+                    <img src="./../Images/Dice${choice.dieFace}.png"/>
+                </div> 
+            `)
+            .join('')
+
+        myBidContainer.innerHTML = bidChoicesMarkup
+
+
+        console.log(document.getElementsByClassName('ind-bids'))
+        console.log(document.getElementsByClassName('ind-bids')[0])
+        // .forEach(element => {
+        //     element.addEventListener('click', this.updateBid)
+        // })
+        document.getElementsByClassName('ind-bids')[0].addEventListener('click', this.updateBid)
+
+        // onclick="updateDiceState(1,2)"
+
+        // function updateDiceState(numOfDice, dieFace) {
+        //     const choice = bidChoices.find(choice => choice.numOfDice === numOfDice && choice.dieFace === dieFace)
+        //     choice.isSelected = true;
+        //     return choice?
     }
-    
+
+    updateBid() {
+        console.log('updating bid')
+        // find the correct choice in this.bidChoices and update the isSelected boolean value
+        // this.showMyAvailableBid()
+    }
+
+
 }
