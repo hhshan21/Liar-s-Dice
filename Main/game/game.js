a18091dc4790da8d35262fea70b4569b7f0408b8
Huishan <hui_shan91@hotmail.com>
2022-06-26 20:49:58 +0800

    added table for bids

--- Main/game/game.js
+++ Main/game/game.js
@@ -20,9 +20,25 @@ export default class Game {
 
         const totalBidChoices = dicePerPlayer * numOfPlayers;
 
+        let myTableDiv = document.getElementById('myDynamicTable')
+        let table = document.createElement('table')
+        let tableBody = document.createElement('tBody')
+        table.appendChild(tableBody)
         
         for (let i = 1; i <= totalBidChoices; i++) {
+            let tr = document.createElement('tr');
+            
+            tableBody.appendChild(tr)
+
             for (let j = 1; j <= this.totalDiceFace; j++) {
+
+                let td = document.createElement('td');
+                td.appendChild(document.createTextNode("Bids " + i + "x" + j));
+                tr.appendChild(td);
+                const btn = document.createElement('button')
+                // document.getElementsByTagName('td').appendChild(btn)
+
+
                 const diceBid = {
                     numOfDice: i,
                     dieFace: j,
@@ -32,6 +48,8 @@ export default class Game {
                 bidChoices.push(diceBid);
             }
         }
+
+        myTableDiv.appendChild(table)
 
         return bidChoices;
     }
@@ -72,9 +90,9 @@ export default class Game {
         const bidChoicesMarkup = this.bidChoices
             .filter((choice) => !choice.isSelected)
             .map(choice => `
-                <div class="ind-bids" bid-dice-count="${choice.numOfDice}" bid-dice-value="${choice.dieFace}">
+                <div class="ind-bids">
                     <span>${choice.numOfDice} x </span>
-                    <img src="./../Images/Dice${choice.dieFace}.png"/>
+                    <img src="./../Images/Dice${choice.dieFace}.png" data-bid-dice-count="${choice.numOfDice}" data-bid-dice-value="${choice.dieFace}"/>
                 </div> 
             `)
             .join('');
@@ -88,26 +106,36 @@ export default class Game {
 
         // onclick="updateDiceState(1,2)"
 
-        // function updateDiceState(numOfDice, dieFace) {
-        //     const choice = bidChoices.find(choice => choice.numOfDice === numOfDice && choice.dieFace === dieFace)
-        //     choice.isSelected = true;
-        //     return choice?
-
     }
-     // end of showMyAvailableBid()
 
+    // updateDiceState(numOfDice, dieFace) {
+    //     const choice = bidChoices.find(choice => choice.numOfDice === numOfDice && choice.dieFace === dieFace)
+    //     console.log(choice)
+    //     choice.isSelected = true;
+    //     // return choice?
 
-
-    updateBid() {
-        console.log('updating bid')
+    updateBid(event) {
+        console.log('updating bid');
+        // console.log(event)
+        // console.log(event.target.dataset)
+        console.log(event.target.dataset.bidDiceCount)
+        console.log(event.target.dataset.bidDiceValue)
         
-        
-        
-       
+        const dicePerPlayer = this.numOfDicePerPlayer;
+        const numOfPlayers = 2;
 
+        const totalBidChoices = dicePerPlayer * numOfPlayers;
+
+        for (let i = 1; i <= totalBidChoices; i++) {
+            for (let j = 1; j <= this.totalDiceFace; j++) {
+
+                if ( i < bidDiceValue && j < bidDiceValue) {
+
+                } 
         
         // find the correct choice in this.bidChoices and update the isSelected boolean value
         // this.showMyAvailableBid()
+            }
     }
 
     // displayBid(name, chosen) {
@@ -115,27 +143,7 @@ export default class Game {
     //   const str = `${name} bid: ${chosen}`
     //   dom.innerHTML = str
     // }
-
-    testDiplay() {
-        `<table>`
-        for (let i = 1; i <= totalBidChoices; i++) {
-            `<tr>
-
-            </tr>`
-            for (let j = 1; j <= this.totalDiceFace; j++) {
-                const diceBid = {
-                    numOfDice: i,
-                    dieFace: j,
-                    // if already selected, dont want it to be selected again
-                    isSelected: false
-                }
-                bidChoices.push(diceBid);
-            }
-        }
     }
 
-    checkValid() {
-
-    }
 
 }
